0ebcdb464b511e5c35959a722abbb2cb071d9893
shiveenpandita <shiveenpandita@gmail.com>
2020-04-29 15:26:59 +1000

    Add ability to get items in reasonable time
    After this commit, the logic will ensure that we don't get stuck looping over just comments and we return the user to a story or poll in a reasonable time

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const got = require("got");
 const { Random } = require("random-js");
 
 const random = new Random();
+const maxfirstHitNotStoryIterations = 10;
 
 exports.handler = async (event) => {
   const link = await getRandomHnArticleLink();
@@ -16,13 +17,40 @@ exports.handler = async (event) => {
 };
 
 const getRandomHnArticleLink = async () => {
-  let topId = "";
+  let topId = '';
+  let randomItemId = '';
+  let hits = 0;
   const response = await got(
     "https://hacker-news.firebaseio.com/v0/maxitem.json?print=pretty"
   );
+  const topStoriesResponse = await got (
+    'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty'
+  );
   topId = response.body;
-  const randomValue = random.integer(1, topId);
-  console.log(randomValue);
-  const link = "https://news.ycombinator.com/item?id=" + randomValue;
+  while(true) {
+    console.log(hits);
+    randomItemId = random.integer(1, topId);
+    console.log(randomItemId);
+    const itemResponse = await getItemMetadata(randomItemId);
+    console.log(itemResponse);
+    if(itemResponse.type === 'story' || itemResponse.type === 'poll') {
+      break;
+    };
+    if (hits > maxfirstHitNotStoryIterations) {
+      let topStoriesArray = JSON.parse(topStoriesResponse.body); 
+      const randomItemIndex = random.integer(1, topStoriesArray.length-1);
+      randomItemId = topStoriesArray[randomItemIndex];
+      break;
+    }
+    hits++;
+  }
+  const link = "https://news.ycombinator.com/item?id=" + randomItemId;
   return link;
+};
+
+const getItemMetadata = async (id) => {
+  const itemResponse = await got(
+    `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`
+  );
+  return JSON.parse(itemResponse.body);
 };
